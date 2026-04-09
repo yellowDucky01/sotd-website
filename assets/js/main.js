00a6edb551f4ea4a8547edb4ecc30cacf41a055e
Ducky <143404804+yellowDucky01@users.noreply.github.com>
2026-04-09 15:57:24 +0800

    Implement server status and player count display across multiple HTML pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,49 @@ window.onclick = function () {
         .querySelectorAll(".dropdown")
         .forEach((d) => (d.style.display = "none"));
 };
+
+let peakPlayers = localStorage.getItem("peak") || 0;
+
+async function fetchServer() {
+    try {
+        const res = await fetch(
+            "https://api.battlemetrics.com/servers/38593009",
+        );
+        const data = await res.json();
+
+        const attr = data.data.attributes;
+
+        const statusEl = document.getElementById("status");
+        const playersEl = document.getElementById("players");
+        const peakEl = document.getElementById("peak");
+
+        if (attr.status === "online") {
+            statusEl.innerText = "ONLINE";
+            statusEl.style.color = "limegreen";
+        } else {
+            statusEl.innerText = "OFFLINE";
+            statusEl.style.color = "red";
+        }
+
+        const currentPlayers = attr.players;
+        const maxPlayers = attr.maxPlayers;
+        playersEl.innerText = `${currentPlayers} / ${maxPlayers}`;
+
+        if (currentPlayers == 0) {
+            statusEl.innerText = "OFFLINE";
+            statusEl.style.color = "red";
+        }
+        if (currentPlayers > peakPlayers) {
+            peakPlayers = currentPlayers;
+            localStorage.setItem("peak", peakPlayers);
+        }
+        peakEl.innerText = peakPlayers;
+    } catch (err) {
+        const statusEl = document.getElementById("status");
+        statusEl.innerText = "ERROR";
+        statusEl.style.color = "orange";
+    }
+}
+
+fetchServer();
+setInterval(fetchServer, 10000);
